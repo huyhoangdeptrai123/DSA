01e1bebb58ad1902594dd2885271e4d478a6aa0b
N22DCPT108 <atuantran469@gmail.com>
2024-06-09 17:00:26 +0700

    Update dslkvong.h
    Them ham sap xep danh sach

--- dslkvong.h
+++ dslkvong.h
@@ -64,3 +64,74 @@ void duyet_dslk_vong(node_dslk_vong*head)
 		while(tmp != head);
 	}
 }
+
+// Sap xep danh sach
+void swap(Sinhvien_dslk_vong &a, Sinhvien_dslk_vong &b) {
+    Sinhvien_dslk_vong temp = a;
+    a = b;
+    b = temp;
+}
+
+// BubbleSort; Goi ham: bubbleSort(dslk_vong); -> in ra: duyet_dslk_vong(dslk_vong);
+void bubbleSort(node_dslk_vong* head) {
+    if (head == NULL) return;
+    node_dslk_vong *i, *j;
+    bool swapped;
+    do {
+        swapped = false;
+        for (i = head; i->next != head; i = i->next) {
+            j = i->next;
+            if (stof(i->data.diem) > stof(j->data.diem)) {
+                swap(i->data, j->data);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
+
+// SelectionSort; Goi ham: selectionSort(dslk_vong);
+void selectionSort(node_dslk_vong* head) {
+    if (head == NULL) return;
+    node_dslk_vong *i, *j, *min;
+    for (i = head; i->next != head; i = i->next) {
+        min = i;
+        for (j = i->next; j != head; j = j->next) {
+            if (stof(j->data.diem) < stof(min->data.diem)) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            swap(i->data, min->data);
+        }
+    }
+}
+
+// InsertionSort; Goi ham: insertionSort(dslk_vong); -> in ra: duyet_dslk_vong(dslk_vong);
+void insertionSort(node_dslk_vong* &head) {
+    if (head == NULL || head->next == head) return;
+    node_dslk_vong *sorted = NULL, *current = head;
+    do {
+        node_dslk_vong *next = current->next;
+        current->next = current->prev = current;
+        if (sorted == NULL) {
+            sorted = current;
+        } else if (stof(current->data.diem) <= stof(sorted->data.diem)) {
+            current->next = sorted;
+            current->prev = sorted->prev;
+            sorted->prev->next = current;
+            sorted->prev = current;
+            sorted = current;
+        } else {
+            node_dslk_vong *tmp = sorted;
+            while (tmp->next != sorted && stof(tmp->next->data.diem) < stof(current->data.diem)) {
+                tmp = tmp->next;
+            }
+            current->next = tmp->next;
+            current->prev = tmp;
+            tmp->next->prev = current;
+            tmp->next = current;
+        }
+        current = next;
+    } while (current != head);
+    head = sorted;
+}
